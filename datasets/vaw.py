4e1d03f6e2a3f7995dc7c87c9fbe0b11d50cfbec
sayybro <shkim129@korea.ac.kr>
2023-06-30 15:36:07 +0900

    0630

--- datasets/vaw.py
+++ datasets/vaw.py
@@ -15,8 +15,6 @@ from skimage.draw import polygon2mask
 import torch
 import torch.utils.data
 import torchvision
-
-# from hotr.data.datasets import builtin_meta
 import datasets.transforms as T
 from util.box_ops import box_cxcywh_to_xyxy
 from .coco import convert_coco_poly_to_mask
@@ -49,12 +47,6 @@ class VAWDetection(torch.utils.data.Dataset):
 
     def num_attributes(self):
         return len(self._valid_att_ids)
-    ############################################################################
-
-    def convert_bbox(self,bbox): #annotation bbox (c_x,c_y,w,h)-> (x1,y1,x2,y2) for roi align
-        x1, y1, w,h = bbox[0], bbox[1], bbox[2], bbox[3]
-        x2, y2 = x1+w, y1+h  
-        return [x1,y1,x2,y2]
 
     def __len__(self):
         return len(self.annotations)
@@ -68,7 +60,6 @@ class VAWDetection(torch.utils.data.Dataset):
         # guard against no boxes via resizing
         boxes = torch.as_tensor(img_anno['boxes'], dtype=torch.float32).reshape(-1, 4)
 
-
         mask_list = []
         for polygon in img_anno['instance_polygon']:
             if polygon is not None:
@@ -81,24 +72,13 @@ class VAWDetection(torch.utils.data.Dataset):
 
         polygons = img_anno['instance_polygon']
         img_id = str(img_anno['image_id'])
-        # for polygon in polygons:
-        #     if polygon is None:
-        #         torch.ones
-
-        #object_names = img_anno['object_name']
-        #clip_embeds = torch.tensor(img_anno['clip_em'])
 
         if self.img_set == 'train':
             # Add index for confirming which boxes are kept after image transformation
             obj_classes = [(i, self._valid_obj_ids.index(obj)) for i, obj in enumerate(img_anno['category_id'])]
-            # pos_att_classes = [(i, pos_att) for i, pos_att in enumerate(img_anno['pos_att_id'])]
-            # neg_att_classes = [(i, neg_att) for i, neg_att in enumerate(img_anno['neg_att_id'])]
         else:
             obj_classes = [self._valid_obj_ids.index(obj) for obj in img_anno['category_id']]
-            # pos_att_classes = [(i, pos_att) for i, pos_att in enumerate(img_anno['pos_att_id'])]
         obj_classes = torch.tensor(obj_classes, dtype=torch.int64)
-        # pos_att_classes = torch.tensor(pos_att_classes, dtype=torch.int64)
-        # neg_att_classes = torch.tensor(neg_att_classes, dtype=torch.int64) if self.img_set =='train' else None
         num_boxes = len(obj_classes)
         num_attributes = self.num_attributes()
         pos_att_classes = torch.zeros((num_boxes,num_attributes),dtype=torch.float32)
@@ -125,9 +105,6 @@ class VAWDetection(torch.utils.data.Dataset):
             pos_att_classes = pos_att_classes[keep]
             neg_att_classes = neg_att_classes[keep]
             target['masks'] = masks[keep]
-            #target['polygons'] =  polygons
-            #target['object_name'] = object_names
-            #target['clip_em'] = clip_embeds[keep]
             target['labels'] = obj_classes
             target['pos_att_classes'] = pos_att_classes
             target['neg_att_classes'] = neg_att_classes
@@ -138,18 +115,12 @@ class VAWDetection(torch.utils.data.Dataset):
             target['boxes'] = boxes
             if self._transforms is not None:
                 img, target = self._transforms(img, target)
-            #target['masks'] =  masks[keep]
-            #target['boxes'] = boxes
-            #target['clip_em'] = clip_embeds[keep]
             target['labels'] = target['labels'][:, 1]
-            # target['pos_att_classes'] = target['pos_att_classes'][:, 1]
             target['dataset'] = 'vaw'
 
         else:
             target['masks'] =  masks
             target['polygons'] =  polygons
-            # target['object_name'] = object_names
-            #target['clip_em'] = clip_embeds
             target['labels'] = obj_classes
             target['pos_att_classes'] = pos_att_classes
             target['id'] = idx
